d5282f081051afe7c037993824d3457e2a6995b1
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-17 10:22:33 +0530

    Update Day1Project.cs

--- Day1Project.cs
+++ Day1Project.cs
@@ -1,1 +1,12 @@
 
+step 1 : create and open asp.net core web api project and give some name like ResortAPI 
+
+step 2: Add the following packages in the project 
+
+Microsoft.EntityFrameworkCore
+Microsoft.EntityFrameworkCore.SqlServer
+Microsoft.EntityFrameworkCore.Tools
+Microsoft.AspNetCore.Identity.EntityFrameworkCore    of version 8.0.24
+Microsoft.AspNetCore.Authentication.JwtBearer     of version 8.0.24
+
+
