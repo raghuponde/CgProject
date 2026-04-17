10d80d102358a71065dfecb6acd2910aec1b6718
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-17 10:28:28 +0530

    Update Day1Project.cs

--- Day1Project.cs
+++ Day1Project.cs
@@ -10,3 +10,9 @@ Microsoft.AspNetCore.Identity.EntityFrameworkCore    of version 8.0.24
 Microsoft.AspNetCore.Authentication.JwtBearer     of version 8.0.24
 
 
+step 3 : go to app settings and set the sql settings like this for the project after putting , add this 
+
+ "AllowedHosts": "*",
+ "ConnectionStrings": {
+   "constring": "Data Source=LAPTOP-4G8BHPK9\\SQLEXPRESS;initial catalog=resortdb;Integrated Security=true;Encrypt=true;TrustServerCertificate=true;"
+ }
