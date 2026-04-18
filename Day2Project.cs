130b780625eed0ee96a73fd64152a3b5afc8e4ed
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-18 10:03:24 +0530

    Update Day2Project.cs

--- Day2Project.cs
+++ Day2Project.cs
@@ -1,1 +1,22 @@
 
+go to project in Models folder add this class 
+
+  using System.ComponentModel.DataAnnotations;
+
+namespace ResortAPI.Models
+{
+    public class RegisterUser
+    {
+
+        [Required(ErrorMessage = "User Name is required")]
+        public string? Username { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+    }
+}
+
+
