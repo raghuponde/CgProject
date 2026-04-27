f4256a100afb3bcc5da157d18083588542c2eafe
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-27 23:27:04 +0530

    Update Day3Project.cs

--- Day3Project.cs
+++ Day3Project.cs
@@ -1283,6 +1283,62 @@ namespace ModifiedProject.Services
     }
 }
 
+and just check my program.cs file as well 
+
+
+using Microsoft.EntityFrameworkCore;
+using ModifiedProject.Models;
+using ModifiedProject.Services;
+
+namespace ModifiedProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddHttpContextAccessor();
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            builder.Services.AddScoped<IEmployee, EmployeeService>();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
+
 so Now my web api is ready let us see how it is working 
 
 
