b0add9ca8dcff1a1fa46c16e9b3c95003bdee608
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-27 14:22:59 +0530

    Update Day3Project.cs

--- Day3Project.cs
+++ Day3Project.cs
@@ -1,1 +1,11 @@
+create an web api project in Day 75 folder 
+
+
+step 2: Add the following packages in the project of version all 8.0.24
+
+Microsoft.EntityFrameworkCore
+Microsoft.EntityFrameworkCore.SqlServer
+Microsoft.EntityFrameworkCore.Tools
+Microsoft.AspNetCore.Identity.EntityFrameworkCore  
+Microsoft.AspNetCore.Authentication.JwtBearer   
 
