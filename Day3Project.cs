fa22c56a7e3282e49686cf6849d2517eacdf9572
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-27 14:47:54 +0530

    Update Day3Project.cs

--- Day3Project.cs
+++ Day3Project.cs
@@ -34,4 +34,55 @@ step 4 :
 	
 	Scaffold-DbContext 'Data Source=LAPTOP-4G8BHPK9\SQLEXPRESS;initial catalog=EmpModified;Integrated Security=true;TrustServerCertificate=True;' Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context ApplicationDbContext -Force
 
+step 5:
+---------
+	Modify the applciation class with this one 
+
+	using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace ModifiedProject.Models;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SeedRoles(modelBuilder);
+    }
+
+    private static void SeedRoles(ModelBuilder builder)
+    {
+        builder.Entity<IdentityRole>().HasData(
+            new IdentityRole()
+            {
+                Name = "Admin",
+                ConcurrencyStamp = "1",
+                NormalizedName = "Admin"
+            },
+            new IdentityRole()
+            {
+                Name = "User",
+                ConcurrencyStamp = "2",
+                NormalizedName = "User"
+            },
+            new IdentityRole()
+            {
+                Name = "HR",
+                ConcurrencyStamp = "3",
+                NormalizedName = "HR"
+            }
+        );
+    }
+}
 
