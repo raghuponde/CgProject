4d9d4d3a673846f019f4d3e5f8e355cafb4d705c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-29 00:51:04 +0530

    Update Day4Project.cs

--- Day4Project.cs
+++ Day4Project.cs
@@ -1,3 +1,11 @@
 
 you have to be in this link only to further continue with the project and to give inputs means u should not change the window as it will not give you complete information 
 I am saying you to be in track with this https://www.perplexity.ai/search/6ee15a24-5b5a-444f-bd35-b555a288cfd4  
+
+Now again open the project which u have done in Day61 to give some features of it now i am going for front end design of my code which is there now in Day 76 ModifiedProject okay 
+so create a new folder now with name Day 77 and in this add Day 76 project as from where to where you have moved some tracking idea will be there so now from 
+  Day 77 open the project okay and start doing coding 
+
+---------------------------------------Design Prompt ------------------------------------------------------------------------------------------
+
+  
